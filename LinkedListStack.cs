da508437d61c885d6de7656fa82047a6e034d006
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-20 22:05:02 +0530

    created a stack push

---
+++ LinkedListStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProgramStackAndQueue
+{
+    public class LinkedListStack
+    {
+        public Node head;
+        //public void Push(int data)
+        //{
+        //    Add(data);
+        //}
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = head;
+            }
+            head = newNode;
+            Console.WriteLine("{0} pushed to stack", data);
+        }
+        
+        public void Display()
+        {
+            Console.WriteLine("Item in the stack");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Data Structure Programs");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
