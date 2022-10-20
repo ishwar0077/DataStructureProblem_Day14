c54f0d06021f69cbed5d2386ba62c37131ffcea4
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-20 21:22:33 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructureProgramStackAndQueue
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Data Structure Programs");
+        }
+    }
+}
